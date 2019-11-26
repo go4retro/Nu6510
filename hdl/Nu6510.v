8b2d45c7a317c8d76950693abc7264a46f8553b8
go4retro <go4retro@go4retro.com>
2019-11-26 13:31:40 -0600

    Support reading and writing all 8 bits of the IO register

--- hdl/Nu6510.v
+++ hdl/Nu6510.v
@@ -33,8 +33,9 @@ module Nu6510(input _reset,
 wire ce_pio;
 wire ce_0000;
 wire ce_0001;
-reg [5:0]data_pio;
-reg [5:0]ddr_pio;
+reg [7:0]data_pio;
+reg [7:0]ddr_pio;
+wire [7:0]pio_out;
 reg [7:0]data_6502_out;
 reg [7:0]data_6510_out;
 
@@ -46,12 +47,16 @@ assign address_6510 =      (aec & clock ? address_6502 : 16'bz);
 assign r_w_6510 =          (aec & clock ? r_w_6502 : 'bz);
 assign data_6502 =         data_6502_out;
 assign data_6510 =         data_6510_out;
-assign pio[5] =            (ddr_pio[5] ? data_pio[5] : 'bz);
-assign pio[4] =            (ddr_pio[4] ? data_pio[4] : 'bz);
-assign pio[3] =            (ddr_pio[3] ? data_pio[3] : 'bz);
-assign pio[2] =            (ddr_pio[2] ? data_pio[2] : 'bz);
-assign pio[1] =            (ddr_pio[1] ? data_pio[1] : 'bz);
-assign pio[0] =            (ddr_pio[0] ? data_pio[0] : 'bz);
+assign pio =               pio_out[5:0];
+
+assign pio_out[7] =        (ddr_pio[7] ? data_pio[7] : 'bz);
+assign pio_out[6] =        (ddr_pio[6] ? data_pio[6] : 'bz);
+assign pio_out[5] =        (ddr_pio[5] ? data_pio[5] : 'bz);
+assign pio_out[4] =        (ddr_pio[4] ? data_pio[4] : 'bz);
+assign pio_out[3] =        (ddr_pio[3] ? data_pio[3] : 'bz);
+assign pio_out[2] =        (ddr_pio[2] ? data_pio[2] : 'bz);
+assign pio_out[1] =        (ddr_pio[1] ? data_pio[1] : 'bz);
+assign pio_out[0] =        (ddr_pio[0] ? data_pio[0] : 'bz);
 
 always @(*)
 begin
@@ -68,12 +73,12 @@ begin
    else if (aec & ce_0000 & clock & r_w_6502) // read PIO
    begin
       data_6510_out = 8'bz;
-      data_6502_out = {2'b0, pio[5:0]};
+      data_6502_out = pio_out;
    end
    else if (aec & ce_0001 & clock & r_w_6502) // read ddr
    begin
       data_6510_out = 8'bz;
-      data_6502_out = {2'b0, ddr_pio[5:0]};
+      data_6502_out = ddr_pio;
    end
    else
    begin
@@ -91,11 +96,11 @@ begin
    end
    else if(!r_w_6502 & ce_0000)
    begin
-      ddr_pio <= {data_6502[5:0]};
+      ddr_pio <= data_6502;
    end
    else if(!r_w_6502 & ce_0001)
    begin
-      data_pio <= {data_6502[5:0]};
+      data_pio <= data_6502;
    end
 end
 
